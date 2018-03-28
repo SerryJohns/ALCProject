111ced0d8ae776444af907e6f1399b51cc7b1851
SerryJohns <serembaj@gmail.com>
2018-03-28 10:48:20 +0100

    Add helper methods and assets

---
+++ app/src/main/java/com/serionz/alcproject/androidme/ui/MasterListAdapter.java
@@ -0,0 +1,55 @@
+package com.serionz.alcproject.androidme.ui;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+
+import java.util.List;
+
+/**
+ * Created by johnpaulseremba on 27/03/2018.
+ */
+
+public class MasterListAdapter extends BaseAdapter {
+
+    // Keeps track of the context and list of images to display
+    private Context mContext;
+    private List<Integer> mImageIds;
+
+    public MasterListAdapter(Context context, List<Integer> imageIds) {
+        this.mContext = context;
+        this.mImageIds = imageIds;
+    }
+
+    @Override
+    public int getCount() {
+        return mImageIds.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mImageIds.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+        if (convertView == null) {
+            imageView = new ImageView(mContext);
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(8, 8, 8, 8);
+        } else {
+            imageView = (ImageView) convertView;
+        }
+        imageView.setImageResource(mImageIds.get(position));
+        return imageView;
+    }
+
+}
