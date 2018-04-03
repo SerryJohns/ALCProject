f419d302fb90e7e9c450d1d765dcf96be9c6c6ce
SerryJohns <serembaj@gmail.com>
2018-04-03 11:55:18 +0100

    Add body parts fragments

--- app/src/main/java/com/serionz/alcproject/androidme/ui/AndroidMeActivity.java
+++ app/src/main/java/com/serionz/alcproject/androidme/ui/AndroidMeActivity.java
@@ -1,15 +1,45 @@
 package com.serionz.alcproject.androidme.ui;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
 import com.serionz.alcproject.R;
+import com.serionz.alcproject.androidme.data.AndroidImageAssets;
 
 public class AndroidMeActivity extends AppCompatActivity {
+    private static final String TAG = AndroidMeActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_android_me);
+
+        if (savedInstanceState == null) {
+            BodyPartFragment headFragment = new BodyPartFragment();
+            headFragment.setImageIds(AndroidImageAssets.getHeads());
+            headFragment.setImageIndex(1);
+
+            FragmentManager fragmentManager = getSupportFragmentManager();
+
+            // Display Head Fragment
+            fragmentManager.beginTransaction()
+                    .add(R.id.container_head, headFragment)
+                    .commit();
+
+            // Display Body Fragment
+            BodyPartFragment bodyFragment = new BodyPartFragment();
+            bodyFragment.setImageIds(AndroidImageAssets.getBodies());
+            fragmentManager.beginTransaction()
+                    .add(R.id.container_body, bodyFragment)
+                    .commit();
+
+            // Display Leg Fragment
+            BodyPartFragment legFragment = new BodyPartFragment();
+            legFragment.setImageIds(AndroidImageAssets.getLegs());
+            fragmentManager.beginTransaction()
+                    .add(R.id.container_leg, legFragment)
+                    .commit();
+        }
     }
+
 }
--- app/src/main/java/com/serionz/alcproject/androidme/ui/BodyPartFragment.java
+++ app/src/main/java/com/serionz/alcproject/androidme/ui/BodyPartFragment.java
@@ -1,110 +1,77 @@
 package com.serionz.alcproject.androidme.ui;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.serionz.alcproject.R;
 
+import java.util.ArrayList;
+import java.util.List;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link BodyPartFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link BodyPartFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class BodyPartFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
+    private static final String TAG = BodyPartFragment.class.getSimpleName();
+    private static final String IMAGE_IDS = "image_ids_list";
+    private static final String LIST_INDEX = "list_index";
+    private List<Integer> mImageIds;
+    private int mListIndex;
 
     public BodyPartFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment BodyPartFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static BodyPartFragment newInstance(String param1, String param2) {
-        BodyPartFragment fragment = new BodyPartFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_body_part, container, false);
-    }
+        final ImageView mImageView;
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
+        if (savedInstanceState != null) {
+            mImageIds = savedInstanceState.getIntegerArrayList(IMAGE_IDS);
+            mListIndex = savedInstanceState.getInt(LIST_INDEX);
         }
-    }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
+        View view = inflater.inflate(R.layout.fragment_body_part, container, false);
+        mImageView = (ImageView) view.findViewById(R.id.img_head);
+
+        if (mImageIds != null) {
+            mImageView.setImageResource(mImageIds.get(mListIndex));
         } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
+            Log.e(TAG, "Image List is empty!");
         }
+
+        mImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mListIndex < mImageIds.size() - 1) {
+                    mListIndex++;
+                } else {
+                    mListIndex = 0;
+                }
+                mImageView.setImageResource(mImageIds.get(mListIndex));
+            }
+        });
+
+        return view;
+    }
+
+    public void setImageIds(List<Integer> imageIds) {
+        this.mImageIds = imageIds;
+    }
+
+    public void setImageIndex(int listIndex) {
+        this.mListIndex = listIndex;
     }
 
     @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putIntegerArrayList(IMAGE_IDS, (ArrayList<Integer>) mImageIds);
+        outState.putInt(LIST_INDEX, mListIndex);
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
 }
